d15b41f0f8ce023ef37472b16443a058970de514
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2022-04-29 19:43:55 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# this calculator is made to calculate your age if you forgot
+# done by : Yara elsakka
+
+current_year = input("please enter your current year: ")
+dob = input("please enter your date of birth: ")
+age = int(current_year) - int(dob)
+
+print(f"in this year {current_year}, you will be turning {age}")
+
+# end of code
